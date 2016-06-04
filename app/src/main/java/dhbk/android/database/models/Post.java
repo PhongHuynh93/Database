c28121ca20ef5f9d20cb4c2c4a8ecceac967d120
Phong <p7157568@gmail.com>
2016-06-04 12:47:26 +0700

    feat - define 2 models: Post and User

---
+++ app/src/main/java/dhbk/android/database/models/Post.java
@@ -0,0 +1,9 @@
+package dhbk.android.database.models;
+
+/**
+ * Created by huynhducthanhphong on 6/4/16.
+ */
+public class Post {
+    public User user;
+    public String text;
+}
---
+++ app/src/main/java/dhbk/android/database/models/User.java
@@ -0,0 +1,9 @@
+package dhbk.android.database.models;
+
+/**
+ * Created by huynhducthanhphong on 6/4/16.
+ */
+public class User {
+    public String userName;
+    public String profilePictureUrl;
+}
