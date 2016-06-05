c9c90768c4a1c16b6f80299338d8e63dcd5cc7be
Phong <p7157568@gmail.com>
2016-06-05 15:21:40 +0700

    feat - add read storage permission on Android 6.0 (api 23)

--- app/src/main/java/dhbk/android/database/activities/MainActivity.java
+++ app/src/main/java/dhbk/android/database/activities/MainActivity.java
@@ -1,15 +1,26 @@
 package dhbk.android.database.activities;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresPermission;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import dhbk.android.database.R;
 import dhbk.android.database.fragment.LoginFragment;
@@ -19,10 +30,11 @@ import dhbk.android.database.models.User;
 import dhbk.android.database.utils.Constant;
 import dhbk.android.database.utils.DatabaseUserHelper;
 
-public class MainActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener, RegisterFragment.OnFragmentInteractionListener, ShowPostFragment.OnFragmentInteractionListener{
+public class MainActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener, RegisterFragment.OnFragmentInteractionListener, ShowPostFragment.OnFragmentInteractionListener {
     private static final String TAG_LOGIN_FRAGMENT = "login_fragment";
     private static final String TAG_SHOW_POST_FRAGMENT = "show_post_fragment";
     private static final String TAG_REGISTER_FRAGMENT = "register_fragment";
+    private static final int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +62,7 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnF
         // TODO: 6/4/16 check infor in edt is the same as database
         Fragment fragment = getSupportFragmentManager().findFragmentByTag(TAG_LOGIN_FRAGMENT);
         if (fragment instanceof LoginFragment) {
-            User userAccount = ((LoginFragment)fragment).checkUserAccount(emailText, passText);
+            User userAccount = ((LoginFragment) fragment).checkUserAccount(emailText, passText);
 
             if (userAccount != null) {
                 // authen success -> save to sharepreference to not type again
@@ -86,7 +98,7 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnF
 
     // replace register fragment with login fragment, fragment login is in front of, and we can autocomplete with field in registerfragment
     @Override
-    public void onRegisterFragmentInteractionAutoComplete(@NonNull String name,@NonNull String email,@NonNull String pass) {
+    public void onRegisterFragmentInteractionAutoComplete(@NonNull String name, @NonNull String email, @NonNull String pass) {
         Fragment fragmentRegister = getSupportFragmentManager().findFragmentByTag(TAG_REGISTER_FRAGMENT);
         if (fragmentRegister instanceof RegisterFragment) {
             getSupportFragmentManager().popBackStack();
@@ -94,14 +106,14 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnF
 
         Fragment fragmentLogin = getSupportFragmentManager().findFragmentByTag(TAG_LOGIN_FRAGMENT);
         if (fragmentLogin instanceof LoginFragment) {
-            ((LoginFragment)fragmentLogin).setEmail(email);
-            ((LoginFragment)fragmentLogin).setPass(pass);
+            ((LoginFragment) fragmentLogin).setEmail(email);
+            ((LoginFragment) fragmentLogin).setPass(pass);
         }
     }
 
     // add data in registerfragment to database
     @Override
-    public void onRegisterFragmentInteractionAddDatabase(@NonNull String name,@NonNull String email,@NonNull String pass) {
+    public void onRegisterFragmentInteractionAddDatabase(@NonNull String name, @NonNull String email, @NonNull String pass) {
         User user = new User(name, email, pass);
         DatabaseUserHelper databaseUserHelper = DatabaseUserHelper.getInstance(this.getApplicationContext());
         databaseUserHelper.addUserToDatabase(user);
@@ -113,14 +125,59 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnF
 
     }
 
-    // post image to showpostfragment
+    // post image to showpostfragment when click button add on toolbar
     @Override
     public void onPostImageToRecyclerView() {
-        getImageFromGaleryFolder();
+        if (Build.VERSION.SDK_INT >= 23) {
+            checkPermissions();
+        }
     }
 
+    @TargetApi(Build.VERSION_CODES.M)
+    private void checkPermissions() {
+        List<String> permissions = new ArrayList<>();
+        String message = "Database permissions:";
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+            message += "\nStorage access to get image from Galery.";
+        }
+
+        if (!permissions.isEmpty()) {
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            String[] params = permissions.toArray(new String[permissions.size()]);
+            requestPermissions(params, REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+        } else {
+            getImageFromGaleryFolder();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS: {
+                Map<String, Integer> perms = new HashMap<>();
+                perms.put(Manifest.permission.READ_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+                for (int i = 0; i < permissions.length; i++)
+                    perms.put(permissions[i], grantResults[i]);
+                Boolean storage = perms.get(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+
+                if (storage) {
+                    Toast.makeText(this, "All permissions granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Storage permission is required to get image from galery", Toast.LENGTH_SHORT).show();
+                }
+            }
+            break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+// END PERMISSION CHECK
+
+    @RequiresPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
     private void getImageFromGaleryFolder() {
-        Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(i, Constant.RESULT_LOAD_IMAGE);
     }
 
@@ -128,11 +185,10 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnF
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-
         if (requestCode == Constant.RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
             Fragment fragmentLogin = getSupportFragmentManager().findFragmentByTag(TAG_SHOW_POST_FRAGMENT);
             if (fragmentLogin instanceof ShowPostFragment) {
-                ((ShowPostFragment)fragmentLogin).addImageToImgView(data);
+                ((ShowPostFragment) fragmentLogin).addImageToImgView(data);
             }
         }
     }
